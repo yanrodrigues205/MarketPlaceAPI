c80b22aa95b7435282346ffd9a630d850d045075
yanrodrigues205 <yan.pablo205@gmail.com>
2023-08-08 15:32:44 -0300

    adicao do controllador de produtos e loja

---
+++ src/controller/ProdutosController.ts
@@ -0,0 +1,23 @@
+import { Request , Response } from "express";
+import { prisma } from "../database/prisma";
+
+export const criarProduto = async (req: Request, res: Response) => {
+    const { nome, preco, quantidade } = req.body;
+
+
+    if(!nome  || !preco  || !quantidade)
+    {
+        return res.status(422).json({ mensagem: "Para cadastrar preencha todos os campos!"});
+    }
+    else
+    {
+        const produto = await prisma.produtos.create({
+            data: { nome , preco, quantidade }
+        });
+
+        return res.json(produto);
+    
+    }
+    
+
+}
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { criarUsuario } from "./controller/UsuarioController";
-
+import { criarProduto  } from "./controller/ProdutosController";
 export const rotas = Router();
 
 rotas.post("/usuario", criarUsuario);
+rotas.post("/cadastrar_produto", criarProduto);
