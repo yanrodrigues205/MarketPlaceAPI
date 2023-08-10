f68ac5ad2e0197c9d0523dfcb51ca36de0a00af2
yanrodrigues205 <yan.pablo205@gmail.com>
2023-08-09 22:08:29 -0300

    tratamento de erros para criar loja

--- src/controller/LojaController.ts
+++ src/controller/LojaController.ts
@@ -5,17 +5,38 @@ export const criarLoja = async (req: Request, res: Response) => {
     const { nome } = req.body;
     const { usuarioID } = req.params;
 
+    const verificaID = await prisma.usuario.findUnique({
+        where: {
+            id: usuarioID
+        }
+    })
 
-    const loja = await prisma.loja.create({
-            data: {
-                nome,
-                Usuario: {
-                    connect: {
-                        id: usuarioID
+    if(!verificaID)
+    {
+        return res.status(422).json({ mensagem: "Usuario nao identificado!"});
+    }
+    else
+    {   
+        if(!nome)
+        {
+            return res.status(422).json({ mensagem: "Preencha todos os campos para criar uma loja!"});
+        }
+        else
+        {
+            const loja = await prisma.loja.create({
+                data: {
+                    nome,
+                    Usuario: {
+                        connect: {
+                            id: usuarioID
+                        }
                     }
                 }
-            }
-           
-        });
+                   
+            });
+        }
+        
+    }
+    
 
 };
