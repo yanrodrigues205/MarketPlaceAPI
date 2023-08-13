4dda9c07cbc73711fa4e568bdd51321fa7740e0d
yanrodrigues205 <yan.pablo205@gmail.com>
2023-08-13 18:46:29 -0300

    protegendo rotas e add tipo de acesso as mesmas

--- src/rotas.ts
+++ src/rotas.ts
@@ -4,6 +4,7 @@ import { criarProduto, pegarProdutos  } from "./controller/ProdutosController";
 import { criarAcesso, pegarAcessos } from "./controller/AcessoController";
 import { criarLoja, pegarLojas } from "./controller/LojaController";
 import { logar } from "./controller/SessaoController";
+import { autenticaMiddleware } from "./middleware/AuthMiddleware";
 
 
 export const rotas = Router();
@@ -16,18 +17,18 @@ rotas.get("/", (req, res) => { // rota inicial
 rotas.post("/logar", logar);
 
 //USUARIO
-rotas.post("/cadastrar_usuario", criarUsuario);
-rotas.delete("/deletar_todos_usuarios", deletarUsuarios);
-rotas.get("/buscar_usuarios", pegarUsuarios);
+rotas.post("/cadastrar_usuario", autenticaMiddleware(["Adm"]), criarUsuario);
+rotas.delete("/deletar_todos_usuarios", autenticaMiddleware(["Adm"]), deletarUsuarios);
+rotas.get("/buscar_usuarios",autenticaMiddleware(["Adm"]), pegarUsuarios);
 
 //PRODUTOS
-rotas.post("/cadastrar_produto", criarProduto);
-rotas.get("/buscar_produtos", pegarProdutos)
+rotas.post("/cadastrar_produto",autenticaMiddleware(["Adm", "Vendedor"]), criarProduto);
+rotas.get("/buscar_produtos",autenticaMiddleware(["Adm", "Vendedor", "Comprador"]),  pegarProdutos)
 
 //ACESSO
-rotas.post("/cadastrar_acesso", criarAcesso);
-rotas.get("/buscar_acessos", pegarAcessos);
+rotas.post("/cadastrar_acesso", autenticaMiddleware(["Adm", "Vendedor", "Comprador"]), criarAcesso);
+rotas.get("/buscar_acessos", autenticaMiddleware(["Adm", "Comprador"]), pegarAcessos);
 
 //LOJA
-rotas.post("/cadastrar_loja", criarLoja);
-rotas.get("/buscar_lojas", pegarLojas);
+rotas.post("/cadastrar_loja",autenticaMiddleware(["Adm", "Vendedor"]), criarLoja);
+rotas.get("/buscar_lojas",autenticaMiddleware(["Adm", "Vendedor", "Comprador"]), pegarLojas);
